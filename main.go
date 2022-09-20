707b7758abc874441a8cfd8e8e6c149065fc3279
Kenneth Fan <kf6999@rit.edu>
2022-09-20 10:37:19 -0700

    HTTP response headers

--- main.go
+++ main.go
@@ -1,107 +1,73 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
 	database "github.com/kf6999/http_server_golang/internal"
 	"log"
 	"net/http"
+	"time"
 )
 
+type errorBody struct {
+	Error string `json:"error"`
+}
+
 func main() {
-	c := database.NewClient("db.json")
-	err := c.EnsureDB()
-	if err != nil {
-		log.Fatal(err)
+	m := http.NewServeMux()
+
+	m.HandleFunc("/", testHandler)
+	m.HandleFunc("/err", testErrHandler)
+
+	const addr = "localhost:8080"
+	srv := http.Server{
+		Handler:      m,
+		Addr:         addr,
+		WriteTimeout: 30 * time.Second,
+		ReadTimeout:  30 * time.Second,
 	}
 
-	user, err := c.CreateUser("test@example.com", "password", "john doe", 18)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("user created", user)
-
-	updatedUser, err := c.UpdateUser("test@example.com", "password", "john doe", 18)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("user updated", updatedUser)
-
-	gotUser, err := c.GetUser("test@example.com")
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("user got", gotUser)
-
-	err = c.DeleteUser("test@example.com")
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("user deleted")
-
-	_, err = c.GetUser("test@example.com")
-	if err == nil {
-		log.Fatal("shouldn't be able to get user that was deleted")
-	}
-	fmt.Println("user confirmed deleted")
-
-	user, err = c.CreateUser("test@example.com", "password", "john doe", 18)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("user recreated", user)
-
-	post, err := c.CreatePost("test@example.com", "my cat is way too fat")
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("post created", post)
-
-	secondPost, err := c.CreatePost("test@example.com", "my cat is getting skinny now")
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("another post created", secondPost)
-
-	posts, err := c.GetPosts("test@example.com")
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("got posts", posts)
-
-	err = c.DeletePost(post.ID)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("deleted first post", posts)
-
-	posts, err = c.GetPosts("test@example.com")
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("got posts", posts)
-
-	err = c.DeletePost(secondPost.ID)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("deleted second post", posts)
-
-	posts, err = c.GetPosts("test@example.com")
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("got posts", posts)
-
-	err = c.DeleteUser("test@example.com")
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("user redeleted")
-
+	// this blocks forever, until the server
+	// has an unrecoverable error
+	fmt.Println("server started on ", addr)
+	err := srv.ListenAndServe()
+	log.Fatal(err)
 }
 
 func testHandler(w http.ResponseWriter, r *http.Request) {
+	respondWithJSON(w, 200, database.User{
+		Email: "test@example.com",
+	})
+}
+
+func testErrHandler(w http.ResponseWriter, r *http.Request) {
+	respondWithError(w, 500, errors.New("server error"))
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(200)
-	w.Write([]byte("{}"))
+	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	if payload != nil {
+		response, err := json.Marshal(payload)
+		if err != nil {
+			log.Println("error marshalling", err)
+			w.WriteHeader(500)
+			response, _ := json.Marshal(errorBody{
+				Error: "error marshalling",
+			})
+			w.Write(response)
+			return
+		}
+		w.WriteHeader(code)
+		w.Write(response)
+	}
+}
+
+func respondWithError(w http.ResponseWriter, code int, err error) {
+	if err == nil {
+		log.Println("dont call respondWithError with nil error")
+	}
+	log.Println(err)
+	respondWithJSON(w, code, errorBody{Error: err.Error()})
 }
